0ed6aedb462a5ca7fe4b580c23c0246527822735
Steven Datt <steven.datt@bashton.com>
2017-03-10 18:50:21 +0000

    corrected issue with security group not creating consistently

--- main.tf
+++ main.tf
@@ -1,14 +1,17 @@
 resource "aws_security_group" "elasticache" {
   name        = "${var.name}"
-  description = "Elasticache security group ${var.name}"
+  description = "Elasticache security group for ${var.name}"
   vpc_id      = "${var.vpc_id}"
-
-  ingress {
-    from_port       = "${var.port}"
-    to_port         = "${var.port}"
-    protocol        = "tcp"
-  }
 }
+
+resource "aws_security_group_rule" "elasticache-in" {
+    type              = "ingress"
+    protocol          = "tcp"
+    from_port         = "${var.port}"
+    to_port           = "${var.port}"
+    security_group_id = "${aws_security_group.elasticache.id}"
+    cidr_blocks       = ["${var.vpc_cidr}"]
+  }
 
 resource "aws_elasticache_subnet_group" "main" {
   name        = "${var.name}"
