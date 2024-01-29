7eb7facd22acc2336f5af48ce799753d9e323bd8
Wayne Clancy <wayneclancy@MacBook-Pro-2020.local>
2024-01-29 10:17:54 +0000

    Fixed 11 vs 12 TF issues -WC

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "endpoint" {
-  value = aws_elasticache_cluster.elasticache.cache_nodes
+  value = aws_elasticache_cluster.elasticache[0].cache_nodes
 }
 
 output "node_address" {
-  value = aws_elasticache_cluster.elasticache.cache_nodes[0].address
+  value = aws_elasticache_cluster.elasticache[0].cache_nodes[0].address
 }
 
 output "memcached_endpoint" {
-  value = aws_elasticache_cluster.elasticache.configuration_endpoint
+  value = aws_elasticache_cluster.elasticache[0].configuration_endpoint
 }
 
