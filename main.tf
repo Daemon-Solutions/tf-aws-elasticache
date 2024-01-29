05f652eb80baf7267a265f21510b0ef2125bec42
Wayne Clancy <wayneclancy@MacBook-Pro-2020.local>
2024-01-29 09:36:17 +0000

    Only run on live -WC

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "elasticache" {
-
+  count = "${var.name == "laguna-live" ? 1 : 0}"
   name = "${var.name}"
   description = "Elasticache security group ${var.name}"
   vpc_id = "${var.vpc_id}"
@@ -14,12 +14,14 @@ resource "aws_security_group" "elasticache" {
 }
 
 resource "aws_elasticache_subnet_group" "main" {
+  count       = "${var.name == "laguna-live" ? 1 : 0}"
   name        = "${var.name}"
   description = "Elasticache subnets for ${var.name}"
   subnet_ids  = ["${split(",", var.subnets)}"]
 }
 
 resource "aws_elasticache_cluster" "elasticache" {
+  count                = "${var.name == "laguna-live" ? 1 : 0}"
   cluster_id           = "${var.name}"
   engine               = "${var.engine}"
   engine_version       = "${var.engine_version}"
